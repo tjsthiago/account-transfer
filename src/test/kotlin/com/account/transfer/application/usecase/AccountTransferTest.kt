9235a073440a414ef0dc395df4449a7576b940be
Thiago José da Silva <tjs.thiago@gmail.com>
2023-06-30 14:56:54 -0300

    refactoring AccountTransferTest

--- src/test/kotlin/com/account/transfer/application/usecase/AccountTransferTest.kt
+++ src/test/kotlin/com/account/transfer/application/usecase/AccountTransferTest.kt
@@ -1,29 +1,36 @@
 package com.account.transfer.application.usecase
 
+import com.account.transfer.application.repository.AccountPersistencePort
 import com.account.transfer.domain.entities.Account
 import com.account.transfer.domain.entities.AccountTransferService
-import com.account.transfer.infra.driven.repository.AccountInMemoryPersistenceAdapter
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootTest()
 class AccountTransferTest {
+
+    @Autowired
+    @Qualifier("AccountInMemoryPersistenceAdapter")
+    private lateinit var accountPersistencePort: AccountPersistencePort
 
     @Test
     fun `should transfer amount between two accounts`() {
-        val accountRepository = AccountInMemoryPersistenceAdapter()
         val accountTransferService = AccountTransferService()
 
         val transferUseCase = AccountTransfer(
-            accountRepository,
+            accountPersistencePort,
             accountTransferService
         )
 
         val from = Account(987654L)
         from.credit(100.0)
-        accountRepository.save(from)
+        accountPersistencePort.save(from)
 
         val to = Account(456123)
-        accountRepository.save(to)
+        accountPersistencePort.save(to)
 
         val amount = 50.0
 
