90946a45b471dc89df58a47d2b42d3dc9421eca1
Thiago José da Silva <tjs.thiago@gmail.com>
2023-07-13 12:46:39 -0300

    fix message consumers

--- src/main/kotlin/com/account/transfer/infra/driver/messaging/input/TransferAmountInputMessage.kt
+++ src/main/kotlin/com/account/transfer/infra/driver/messaging/input/TransferAmountInputMessage.kt
@@ -1,7 +1,14 @@
 package com.account.transfer.infra.driver.messaging.input
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class TransferAmountInputMessage (
+    @JsonProperty("from")
     val from: Long,
+
+    @JsonProperty("to")
     val to: Long,
+
+    @JsonProperty("amount")
     val amount: Double
 )
