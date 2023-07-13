6891d20ede94de63cebe41dc8930b5fd2f284d45
Thiago José da Silva <tjs.thiago@gmail.com>
2023-07-13 11:48:48 -0300

    fix config

--- src/main/kotlin/com/account/transfer/AccountTransferApplication.kt
+++ src/main/kotlin/com/account/transfer/AccountTransferApplication.kt
@@ -1,23 +1,10 @@
 package com.account.transfer
 
-import com.account.transfer.application.repository.AccountPersistencePort
-import com.account.transfer.infra.driven.repository.AccountPersistenceAdapter
-import com.account.transfer.infra.driven.repository.AccountRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class AccountTransferApplication{
-	@Autowired
-	private lateinit var accountRepository: AccountRepository
-
-	@Bean
-	fun getAccountPersistencePort(): AccountPersistencePort {
-		return AccountPersistenceAdapter(accountRepository)
-	}
-
 }
 
 fun main(args: Array<String>) {
